1c3907d6fa6dcd7246a9f9d72a4cdfd7a4ed74e4
Scott <sfrancis@misinteractive.com>
2017-11-25 09:54:59 -0500

    Added extension methods

---
+++ RevStackCore.Extensions.Mvc/Extensions/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Json = RevStackCore.Serialization.Json;
+namespace RevStackCore.Extensions.Mvc
+{
+    public static class MvcExtensions
+    {
+        public static bool IsDefaultTypeValue<T>(this T value)
+        {
+            return EqualityComparer<T>.Default.Equals(value, default(T));
+        }
+
+        public static string ToJsonString<T>(this T src)
+        {
+            if (src.IsDefaultTypeValue()) return null;
+            return Json.SerializeObject(src, true, true);
+        }
+    }
+}
